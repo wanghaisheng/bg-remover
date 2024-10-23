27d75c2c4b3b9c73fbe19956b98f8430b5cb783b
AjitGode <godeajit71@gmail.com>
2024-10-23 17:02:08 +0530

    updated code

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -12,9 +12,9 @@ const clerkWebhooks = async (req,res) =>{
             const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET )
 
             await whook.verify(JSON.stringify(req.body),{
-                "svix-id":req.header["svix-d"],
-                "svix-timestamp": req.header["svix-timestamp"],
-                "svix-signature": req.header["svix-signature"],
+                "svix-id": req.headers["svix-id"],
+                "svix-timestamp": req.headers["svix-timestamp"],
+                "svix-signature": req.headers["svix-signature"],
             })
 
             const {data, type} = req.body
